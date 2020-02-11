baf5bd9ac4b7119e3d8e5abefb332ee1661b0320
Igor Maliy <imaliy.filsh@gmail.com>
2020-02-11 10:53:02 +0200

    Update

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,5 @@
 'use strict';
-import { shallowMount } from '@vue/test-utils';
 import { expect } from 'chai';
-
-// more example https://medium.com/js-dojo/unit-testing-in-vue-mocha-part-2-ebf1df4b8897
 
 describe('_____', () => {
   before(() => {
@@ -21,8 +18,8 @@ describe('_____', () => {
     expect(true).to.be.true;
     expect(false).to.be.false;
     expect(undefined).to.be.undefined;
-    expect([1,2,3]).to.have.property(1);
-    expect(['.js','.jsx','.vue']).to.include.members(['.js','.jsx']);
-    expect({id: 1, name: 'sky'}).to.include.all.keys(['id', 'name']);
+    expect([1, 2, 3]).to.have.property(1);
+    expect(['.js', '.jsx', '.vue']).to.include.members(['.js', '.jsx']);
+    expect({ id: 1, name: 'sky' }).to.include.all.keys(['id', 'name']);
   });
 });
